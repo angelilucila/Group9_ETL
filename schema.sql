25f128dccb01b39cecd1da6398d349ef33fb72d4
Angeli Lucila <Angeli@Angelis-MBP.home>
2021-02-15 14:43:29 -0500

    sql

---
+++ schema.sql
@@ -0,0 +1,15 @@
+-- need to update.. taken from class activity 
+
+-- Create Two Tables
+CREATE TABLE premise (
+  id INT PRIMARY KEY,
+  premise_name TEXT,
+  county_id INT
+);
+
+CREATE TABLE county (
+  id INT PRIMARY KEY,
+  county_name TEXT,
+  license_count INT,
+  county_id INT
+);
