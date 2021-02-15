3aadd8585d4509cfe396f7b68ecc4be49df4b48d
Angeli Lucila <Angeli@Angelis-MBP.home>
2021-02-15 14:43:07 -0500

    sql

---
+++ query.sql
@@ -0,0 +1,13 @@
+
+-- need to update.. taken from class example
+
+-- Query to check successful load
+SELECT * FROM premise;
+
+SELECT * FROM county;
+
+-- Join tables on county_id
+SELECT premise.id, premise.premise_name, county.county_name
+FROM premise
+INNER JOIN county
+ON premise.county_id = county.county_id;
